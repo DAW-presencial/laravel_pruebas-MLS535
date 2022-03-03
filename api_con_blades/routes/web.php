818d12d0eda0c9f8dcbc04d8bcd202419e60913f
MLS535 <74008091+MLS535@users.noreply.github.com>
2022-03-03 12:17:10 +0100

    prueba

--- api_con_blades/routes/web.php
+++ api_con_blades/routes/web.php
@@ -16,6 +16,4 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/', function (){
-    return view('musicos.index');
-});
+Route::resource('musicos', MusicoController::class)->names('musician')->parameters(['musician' => 'musico']);
